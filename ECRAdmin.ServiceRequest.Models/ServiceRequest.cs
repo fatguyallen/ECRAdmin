de13ccf65b27fd9e74105d86d0b115b24ee1c1b3
alin.cehan <alin.cehan@theinsurers.co.uk>
2016-03-11 17:07:47 +0200

    Fixed namespace

--- ECRAdmin.ServiceRequest.Models/ServiceRequest.cs
+++ ECRAdmin.ServiceRequest.Models/ServiceRequest.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ECRAdmin.ServiceRequestCRUD.Models
+namespace ECRAdmin.ServiceRequest.Models
 {
     public class ServiceRequest
     {
